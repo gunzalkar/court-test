4c231c2aac98d23728e7ab3eeb78fbc3b35ee45a
gunzalkar <42808290+gunzalkar@users.noreply.github.com>
2022-07-03 01:05:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -174,8 +174,8 @@ class mainWebCrawler:
         self.table.to_csv(f"{self.COURT_NAME} {custom_date}.csv", index=False)
 
 
-_from = '2021/05/01' #YYYY/MM/DD
-_to = '2021/05/31'
+_from = '2021/06/01' #YYYY/MM/DD
+_to = '2021/06/30'
 
 s_split = _from.split("/")
 e_split = _to.split("/")
